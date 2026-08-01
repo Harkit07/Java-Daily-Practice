9d055d3740cf4b8627189f30e6c5048967391a35
harkit singh <harkitsinghsran9584@gmail.com>
2026-08-01 14:59:38 +0530

    write a optimized selectionsort code

--- Sort.java
+++ Sort.java
@@ -16,6 +16,22 @@ public class Sort {
         }
     }
 
+    public static void selectionSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minPos = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[minPos] > arr[j]) {
+                    minPos = j;
+                }
+            }
+            if (minPos != i) {
+                int temp = arr[i];
+                arr[i] = arr[minPos];
+                arr[minPos] = temp;
+            }
+        }
+    }
+
     public static void printArr(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
@@ -25,7 +41,7 @@ public class Sort {
 
     public static void main(String[] args) {
         int arr[] = { 3, 5, 4, 2, 1 };
-        bubbleSort(arr);
+        selectionSort(arr);
         printArr(arr);
     }
 }
