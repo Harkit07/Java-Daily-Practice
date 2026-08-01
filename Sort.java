79e8d9a9b28ea25b4a4e0d8f47c696b75bc2901b
harkit singh <harkitsinghsran9584@gmail.com>
2026-08-01 15:23:06 +0530

    practicing insertion Sort and counting Sort

--- Sort.java
+++ Sort.java
@@ -32,6 +32,39 @@ public class Sort {
         }
     }
 
+    public static void insertionSort(int arr[]) {
+        for (int i = 1; i < arr.length; i++) {
+            int curr = arr[i];
+            int prev = i - 1;
+            while (prev >= 0 && arr[prev] > curr) {
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+            arr[prev + 1] = curr;
+        }
+    }
+
+    public static void countingSort(int arr[]) {
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+
+        int count[] = new int[largest + 1];
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+
+        int j = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+
     public static void printArr(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
@@ -41,7 +74,7 @@ public class Sort {
 
     public static void main(String[] args) {
         int arr[] = { 3, 5, 4, 2, 1 };
-        selectionSort(arr);
+        countingSort(arr);
         printArr(arr);
     }
 }
