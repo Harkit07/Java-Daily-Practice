0fe62869eae89bccf2627571bb0a4218c8801b25
harkit singh <harkitsinghsran9584@gmail.com>
2026-08-04 12:21:06 +0530

    practice getIthBit, setIthBit and clearIthBit

--- BItManupulation.java
+++ BItManupulation.java
@@ -16,10 +16,31 @@ public class BItManupulation {
         System.out.println("This is" + result);
     }
 
+    public static int getIthBit(int n, int i) {
+        int bitMask = 1 << i;
+        if ((n & bitMask) == 0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    public static int setIthBit(int n, int i) {
+        int bitMask = 1 << i;
+        return n | bitMask;
+    }
+
+    public static int clearIthBit(int n, int i) {
+        int bitMask = ~(1 << i);
+        return n & bitMask;
+    }
+
     public static void main(String[] args) {
         // System.out.println((~15));
-        oddOrEven(8);
-        oddOrEven(9);
+        // oddOrEven(8);
+        // oddOrEven(9);
+        // System.out.println(getIthBit(10, 3));
         // System.out.println(setIthBit(10, 2));
+        System.out.println(clearIthBit(6, 1));
     }
 }
