ce97137d82bd283204ff48dec48d17190ec47f96
harkit singh <harkitsinghsran9584@gmail.com>
2026-08-04 12:12:06 +0530

    practive bit manupulation

--- BItManupulation.java
+++ BItManupulation.java
@@ -1,31 +1,25 @@
 public class BItManupulation {
 
-    public static void oddEven(int n) {
-        int bitMask = 1;
-        if ((n & bitMask) == 1) {
-            System.out.println("Odd Number");
-        } else {
-            System.out.println("Even Number");
-        }
-    }
+    // public static void oddOrEven(int n) {
+    // int bitMask = 1;
+    // if ((n & bitMask) == 0) {
+    // System.out.println("This is even");
+    // return;
+    // } else {
+    // System.out.println("This is odd");
+    // return;
+    // }
+    // }
 
-    public static int getIthBit(int n, int i) {
-        int bitMask = 1 << i;
-        if ((n & bitMask) == 0) {
-            return 0;
-        } else {
-            return 1;
-        }
-    }
-
-    public static int setIthBit(int n, int i) {
-        int bitMask = 1 << i;
-        return n | bitMask;
+    public static void oddOrEven(int n) {
+        String result = ((n & 1) == 0) ? "Even" : "Odd";
+        System.out.println("This is" + result);
     }
 
     public static void main(String[] args) {
         // System.out.println((~15));
-        // oddEven(1110);
-        System.out.println(setIthBit(10, 2));
+        oddOrEven(8);
+        oddOrEven(9);
+        // System.out.println(setIthBit(10, 2));
     }
 }
