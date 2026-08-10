817bdbe1422c62e3e760b77d44bd89973bc99ec2
harkit singh <harkitsinghsran9584@gmail.com>
2026-08-10 17:01:31 +0530

    implementation of LinkList in java

---
+++ linkLIst.java
@@ -0,0 +1,38 @@
+
+public class linkLIst {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+
+        public static Node head;
+        public static Node tail;
+        public static Node size;
+    }
+
+    // Add in LinkList - addFirst
+
+    // Add in LinkList - addLast
+
+    // Print LinkList
+
+    // Add in middle - add
+
+    // Size of LinkList
+
+    // Remove in LinkList = removeFirst
+
+    // Remove in LinkList = removeLast
+
+    // Search in LinkList - Iterative
+
+    // Search in LinkList - Recursive
+
+    public static void main(String[] args) {
+
+    }
+}
