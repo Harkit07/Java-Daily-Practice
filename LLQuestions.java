7d9d2cb0dc24c6e56ab643e30b2a40bf8767b517
harkit singh <harkitsinghsran9584@gmail.com>
2026-08-11 07:36:57 +0530

    practice questions

--- LLQuestions.java
+++ LLQuestions.java
@@ -26,8 +26,50 @@ public class LLQuestions {
     }
 
     // Reverse a LinkedList
+    public static Node reverse(Node head) {
+        if (head == null) {
+            System.out.println("Linked List is Empty");
+            return null;
+        } else {
+            Node prev = null;
+            Node curr = tail = head;
+            Node next;
+            while (curr != null) {
+                next = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+            }
+            return prev;
+        }
+    }
 
     // Find and remove Nth node form end (Iterative approach)
+    public static Node deleteNthFromEnd(Node head, int idx) {
+        int sz = 0;
+        Node temp = head;
+        while (temp != null) {
+            sz++;
+            temp = temp.next;
+        }
+
+        if (idx <= 0 || idx > sz) {
+            System.out.println("Invalid index");
+            return head;
+        }
+
+        if (idx == sz) {
+            return head.next;
+        }
+        int i = 1;
+        Node prev = head;
+        while (i < sz - idx) {
+            prev = prev.next;
+            i++;
+        }
+        prev.next = prev.next.next;
+        return head;
+    }
 
     // Check if LL is a Palindrone
 
@@ -41,6 +83,10 @@ public class LLQuestions {
         head.next = new Node(2);
         head.next.next = new Node(3);
 
+        // printLL(head);
+        // head = reverse(head);
+        printLL(head);
+        head = deleteNthFromEnd(head, 2);
         printLL(head);
     }
 }
