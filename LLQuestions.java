748326a89c60f59e34f1ce39ef3bdc5ddb8077e7
harkit singh <harkitsinghsran9584@gmail.com>
2026-08-11 08:09:19 +0530

    practice LinkedList question

--- LLQuestions.java
+++ LLQuestions.java
@@ -72,6 +72,41 @@ public class LLQuestions {
     }
 
     // Check if LL is a Palindrone
+    public static Node findMid(Node head) {
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public static boolean isPalindrome(Node head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        Node midNode = findMid(head);
+        Node prev = null;
+        Node curr = midNode;
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        Node right = prev;
+        Node left = head;
+        while (right != null) {
+            if (left.data != right.data) {
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
         // LinkedList<Integer> LL = new LinkedList<>();
