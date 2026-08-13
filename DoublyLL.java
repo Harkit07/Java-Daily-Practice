c8f21459880b125c5b991e87f01fc9a8482a20e9
harkit singh <harkitsinghsran9584@gmail.com>
2026-08-13 11:15:00 +0530

    implementation of Doubly Linked List

---
+++ DoublyLL.java
@@ -0,0 +1,84 @@
+public class DoublyLL {
+    static class Node {
+        int data;
+        Node prev;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.prev = null;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+        size++;
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    public void printLL() {
+        if (head == null) {
+            System.out.println("LL is Empty");
+            return;
+        } else {
+            Node temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + "->");
+                temp = temp.next;
+            }
+            System.out.println("null");
+        }
+    }
+
+    public int removeLast() {
+        if (head == null) {
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            size--;
+            int val = head.data;
+            head = tail = null;
+            return val;
+        } else {
+            size--;
+            int val = tail.data;
+            tail = tail.prev;
+            tail.next = null;
+            return val;
+        }
+    }
+
+    public static void main(String[] args) {
+        DoublyLL DLL = new DoublyLL();
+        DLL.addFirst(2);
+        DLL.addFirst(1);
+        DLL.addLast(3);
+        DLL.addLast(4);
+        DLL.printLL();
+        DLL.removeLast();
+        DLL.printLL();
+
+    }
+}
