1193d749fe9183cbfb7217b456f84cac1901be0c
harkit singh <harkitsinghsran9584@gmail.com>
2026-08-13 06:59:44 +0530

    zig-zag linkedlist

--- LLQuestions.java
+++ LLQuestions.java
@@ -209,6 +209,37 @@ public class LLQuestions {
     }
 
     // Zig-Zag in a LL
+    public void zigZag(Node head) {
+        Node slow = head;
+        Node fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node mid = slow;
+        Node curr = mid;
+        mid.next = null;
+        Node prev = null;
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        Node left = head;
+        Node right = prev;
+        Node nextL, nextR;
+        while (left != null && right != null) {
+            nextL = left.next;
+            left.next = right;
+            nextR = right.next;
+            right.next = nextL;
+
+            left = nextL;
+            right = nextR;
+        }
+    }
 
     public static void main(String[] args) {
         // LinkedList<Integer> LL = new LinkedList<>();
