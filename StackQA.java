913b202a727b604896e28d348a2b0fb72528fbbd
harkit singh <harkitsinghsran9584@gmail.com>
2026-08-14 13:30:28 +0530

    practive stack high level questions

--- StackQA.java
+++ StackQA.java
@@ -93,6 +93,92 @@ public class StackQA {
         System.out.println();
     }
 
+    // Valid Parentheses
+    public static boolean validParentheses(String str) {
+        Stack<Character> s = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                s.push(ch);
+            } else {
+                if (s.isEmpty()) {
+                    return false;
+                }
+                if (s.peek() == '(' && ch == ')' || s.peek() == '{' && ch == '}' || s.peek() == '[' && ch == ']') {
+                    s.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        if (s.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Duplicate Parentheses
+    public static boolean duplicateParentheses(String str) {
+        Stack<Character> s = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == ')') {
+                int count = 0;
+                while (s.peek() != '(') {
+                    s.pop();
+                    count++;
+                }
+                if (count < 1) {
+                    return true;
+                } else {
+                    s.pop();
+                }
+            } else {
+                s.push(ch);
+            }
+        }
+        return false;
+    }
+
+    // Max Area of Histogram
+    public static int maxAreaHistogram(int arr[]) {
+        int maxArea = 0;
+        int nsr[] = new int[arr.length];
+        int nsl[] = new int[arr.length];
+        Stack<Integer> s = new Stack<>();
+        for (int i = arr.length - 1; i >= 0; i--) {
+            while (!s.isEmpty() && arr[s.peek()] >= arr[i]) {
+                s.pop();
+            }
+            if (s.isEmpty()) {
+                nsr[i] = arr.length;
+            } else {
+                nsr[i] = s.peek();
+            }
+            s.push(i);
+        }
+        s = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            while (!s.isEmpty() && arr[s.peek()] >= arr[i]) {
+                s.pop();
+            }
+            if (s.isEmpty()) {
+                nsl[i] = -1;
+            } else {
+                nsl[i] = s.peek();
+            }
+            s.push(i);
+        }
+        for (int i = 0; i < arr.length; i++) {
+            int height = arr[i];
+            int width = nsr[i] - nsl[i] - 1;
+            int currArea = height * width;
+            maxArea = Math.max(maxArea, currArea);
+        }
+        return maxArea;
+    }
+
     public static void main(String[] args) {
         Stack<Integer> s = new Stack<>();
         s.push(2);
@@ -108,7 +194,13 @@ public class StackQA {
         // int stocks[] = { 100, 80, 60, 70, 60, 85, 100 };
         // int span[] = new int[stocks.length];
         // stocksSpan(stocks, span);
-        int arr[] = { 6, 8, 0, 1, 3 };
-        nextGreater(arr);
+        // int arr[] = { 6, 8, 0, 1, 3 };
+        // nextGreater(arr);
+        String vpTest = "{[()]}";
+        System.out.println(validParentheses(vpTest));
+        String dpTest = "((a+b))";
+        System.out.println(duplicateParentheses(dpTest));
+        int[] histTest = { 6, 2, 5, 4, 5, 1, 6 };
+        System.out.println(maxAreaHistogram(histTest));
     }
 }
