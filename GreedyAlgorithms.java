c6d1fe419a3660b514c5000be3af9f526f99a2f0
harkit singh <harkitsinghsran9584@gmail.com>
2026-08-17 14:54:46 +0530

    write questions of greedy algorithms

--- GreedyAlgorithms.java
+++ GreedyAlgorithms.java
@@ -14,6 +14,32 @@ public class GreedyAlgorithms {
     // Chocola Problem
 
     public static void main(String[] args) {
+        // Activity Selection
+        // int start[] = { 10, 12, 20 };
+        // int end[] = { 20, 25, 30 };
+
+        // Fractional Knapsack
+        // int value[] = { 60, 100, 120 };
+        // int weight[] = { 10, 20, 30 };
+        // int W = 50;
+
+        // Min Absulate Difference Pairs
+        // int A[] = { 1, 2, 3 };
+        // int B[] = { 2, 1, 3 };
+
+        // Max Length Chain of Pairs
+        // int pairs[][] = { { 5, 24 }, { 39, 60 }, { 5, 28 }, { 27, 40 }, { 50, 90 } };
+
+        // Indian Coin
+        // int val = 590;
+
+        // Job Sequencing Problem
+        // int jobsInfo[][] = { { 4, 20 }, { 1, 10 }, { 1, 40 }, { 1, 30 } };
+
+        // Chocola Problem
+        // int n = 4, m = 6;
+        // Integer costVer[] = { 2, 1, 3, 1, 4 };
+        // Integer costHor[] = { 4, 1, 2 };
 
     }
 }
