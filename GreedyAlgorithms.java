bd31710c6c18530002bc421c6dcaf9e255a74415
harkit singh <harkitsinghsran9584@gmail.com>
2026-08-17 14:45:29 +0530

    write questions of greedy algorithms

---
+++ GreedyAlgorithms.java
@@ -0,0 +1,19 @@
+public class GreedyAlgorithms {
+    // Activity Selection
+
+    // Fractional Knapsack
+
+    // Min Absulate Difference Pairs
+
+    // Max Length Chain of Pairs
+
+    // Indian Coin
+
+    // Job Sequencing Problem
+
+    // Chocola Problem
+
+    public static void main(String[] args) {
+
+    }
+}
