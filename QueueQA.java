73f0b0b0ee09883eb6000c83e60c58fff36c18e6
harkit singh <harkitsinghsran9584@gmail.com>
2026-08-17 06:00:07 +0530

    solver question of queue

--- QueueQA.java
+++ QueueQA.java
@@ -1,5 +1,6 @@
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class QueueQA {
     // Stack using 2 Queue
@@ -59,10 +60,48 @@ public class QueueQA {
     // }
 
     // First non-repeating latter in a stream of character
+    public static void printNonRepeating(String str) {
+        Queue<Character> q = new LinkedList<>();
+        int freq[] = new int[26];
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            q.add(ch);
+            freq[ch - 'a']++;
+            while (!q.isEmpty() && freq[q.peek() - 'a'] > 1) {
+                q.remove();
+            }
+            if (q.isEmpty()) {
+                System.out.print(-1 + " ");
+            } else {
+                System.out.print(q.peek() + " ");
+            }
+        }
+        System.out.println();
+    }
 
     // Interleaves 2 halves of a Queue(Even length)
+    public static void interLeave(Queue<Integer> q) {
+        Queue<Integer> firstHalves = new LinkedList<>();
+        int size = q.size();
+        for (int i = 0; i < size / 2; i++) {
+            firstHalves.add(q.remove());
+        }
+        while (!firstHalves.isEmpty()) {
+            q.add(firstHalves.remove());
+            q.add(q.remove());
+        }
+    }
 
     // Queue Reversal
+    public static void reverse(Queue<Integer> q) {
+        Stack<Integer> s = new Stack<>();
+        while (!q.isEmpty()) {
+            s.add(q.remove());
+        }
+        while (!s.isEmpty()) {
+            q.add(s.pop());
+        }
+    }
 
     public static void main(String[] args) {
         // Stack s = new Stack();
@@ -79,10 +118,24 @@ public class QueueQA {
         q.add(1);
         q.add(2);
         q.add(3);
+        q.add(4);
+        q.add(5);
+        q.add(6);
+        // while (!q.isEmpty()) {
+        // System.out.print(q.peek() + " ");
+        // q.remove();
+        // }
+        // System.out.println();
+
+        // String str = "aabccxb";
+        // printNonRepeating(str);
+        // interLeave(q);
+        reverse(q);
         while (!q.isEmpty()) {
             System.out.print(q.peek() + " ");
             q.remove();
         }
         System.out.println();
+
     }
 }
