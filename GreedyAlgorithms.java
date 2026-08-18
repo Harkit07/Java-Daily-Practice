ac34d6a5070560931dd95bab63ddfe7aaf78a997
harkit singh <harkitsinghsran9584@gmail.com>
2026-08-18 13:23:04 +0530

    solve chocola problem hard level

--- GreedyAlgorithms.java
+++ GreedyAlgorithms.java
@@ -144,6 +144,36 @@ public class GreedyAlgorithms {
     }
 
     // Chocola Problem
+    public static int chocolaProblem(Integer costHor[], Integer costVer[]) {
+        Arrays.sort(costVer, Collections.reverseOrder());
+        Arrays.sort(costHor, Collections.reverseOrder());
+
+        int h = 0, v = 0;
+        int hp = 1, vp = 1;
+        int cost = 0;
+        while (h < costHor.length && v < costVer.length) {
+            if (costVer[v] <= costHor[h]) {
+                cost += (costHor[h] * vp);
+                hp++;
+                h++;
+            } else {
+                cost += (costVer[v] * hp);
+                vp++;
+                v++;
+            }
+        }
+        while (h < costHor.length) {
+            cost += (costHor[h] * vp);
+            hp++;
+            h++;
+        }
+        while (v < costVer.length) {
+            cost += (costVer[v] * hp);
+            vp++;
+            v++;
+        }
+        return cost;
+    }
 
     public static void main(String[] args) {
         // Activity Selection
@@ -171,13 +201,13 @@ public class GreedyAlgorithms {
         // indianCoin(val);
 
         // Job Sequencing Problem
-        int jobsInfo[][] = { { 4, 20 }, { 1, 10 }, { 1, 40 }, { 1, 30 } };
-        jobSequencing(jobsInfo);
+        // int jobsInfo[][] = { { 4, 20 }, { 1, 10 }, { 1, 40 }, { 1, 30 } };
+        // jobSequencing(jobsInfo);
 
         // Chocola Problem
         // int n = 4, m = 6;
-        // Integer costVer[] = { 2, 1, 3, 1, 4 };
-        // Integer costHor[] = { 4, 1, 2 };
-
+        Integer costVer[] = { 2, 1, 3, 1, 4 };
+        Integer costHor[] = { 4, 1, 2 };
+        System.out.println(chocolaProblem(costHor, costVer));
     }
 }
