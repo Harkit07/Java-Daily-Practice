a4f2301d11453d9f7750cb8e079d846582356d7f
harkit singh <harkitsinghsran9584@gmail.com>
2026-08-18 15:05:13 +0530

    practice preOrder, postOrder and inOrder traversal

--- BinaryTree.java
+++ BinaryTree.java
@@ -26,9 +26,42 @@ public class BinaryTree {
         }
     }
 
+    public static void preOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    public static void inOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
+
+    public static void postOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data + " ");
+    }
+
     public static void main(String[] args) {
         int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
         Node root = BinaryTrees.buildTree(nodes);
-        System.out.println(root.data);
+        // System.out.println(root.data);
+        preOrder(root);
+        System.out.println();
+        inOrder(root);
+        System.out.println();
+        postOrder(root);
+        System.out.println();
     }
 }
