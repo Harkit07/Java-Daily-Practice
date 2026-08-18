d5228e4f7e348fe8a19acb920ee85fcc0dcb710a
harkit singh <harkitsinghsran9584@gmail.com>
2026-08-18 15:22:03 +0530

    practice levelOrder, count height,sum and node of tree

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinaryTree {
     static class Node {
         int data;
@@ -11,6 +14,7 @@ public class BinaryTree {
         }
     }
 
+    // Create Binary Tree
     static class BinaryTrees {
         static int idx = -1;
 
@@ -26,6 +30,7 @@ public class BinaryTree {
         }
     }
 
+    // Pre-Order Traversal
     public static void preOrder(Node root) {
         if (root == null) {
             return;
@@ -35,6 +40,7 @@ public class BinaryTree {
         preOrder(root.right);
     }
 
+    // In-Order Traversal
     public static void inOrder(Node root) {
         if (root == null) {
             return;
@@ -44,6 +50,7 @@ public class BinaryTree {
         inOrder(root.right);
     }
 
+    // Post-Order Traversal
     public static void postOrder(Node root) {
         if (root == null) {
             return;
@@ -53,15 +60,79 @@ public class BinaryTree {
         System.out.print(root.data + " ");
     }
 
+    // Level-Order Traversal
+    public static void levelOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+        while (!q.isEmpty()) {
+            Node curr = q.remove();
+            if (curr == null) {
+                System.out.println();
+                if (q.isEmpty()) {
+                    break;
+                } else {
+                    q.add(null);
+
+                }
+            } else {
+                System.out.print(curr.data + " ");
+                if (curr.left != null) {
+                    q.add(curr.left);
+                }
+                if (curr.right != null) {
+                    q.add(curr.right);
+                }
+            }
+        }
+    }
+
+    // Count Height of Tree
+    public static int height(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int lh = height(root.left);
+        int rh = height(root.right);
+        return Math.max(lh, rh) + 1;
+    }
+
+    // Count Node of Tree
+    public static int count(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int lc = count(root.left);
+        int rc = count(root.right);
+        return lc + rc + 1;
+    }
+
+    // Count Sum of Tree
+    public static int sum(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int ls = sum(root.left);
+        int rs = sum(root.right);
+        return ls + rs + root.data;
+    }
+
     public static void main(String[] args) {
         int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
         Node root = BinaryTrees.buildTree(nodes);
         // System.out.println(root.data);
-        preOrder(root);
-        System.out.println();
-        inOrder(root);
-        System.out.println();
-        postOrder(root);
-        System.out.println();
+        // preOrder(root);
+        // System.out.println();
+        // inOrder(root);
+        // System.out.println();
+        // postOrder(root);
+        // System.out.println();
+        levelOrder(root);
+        System.out.println(height(root));
+        System.out.println(count(root));
+        System.out.println(sum(root));
     }
 }
