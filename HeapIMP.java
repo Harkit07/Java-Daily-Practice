6b539eaddafef408b12056fd0739617d1b19c771
harkit singh <harkitsinghsran9584@gmail.com>
2026-08-23 17:47:11 +0530

    practice implementation of heap or pirority queues

--- HeapIMP.java
+++ HeapIMP.java
@@ -19,10 +19,62 @@ public class HeapIMP {
             }
         }
 
+        // Peek in Heap
+        public int peek() {
+            return arr.get(0);
+        }
+
+        // Remove in Heap
+        private void heapify(int i) {
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+            int minIdx = i;
+
+            if (left < arr.size() && arr.get(minIdx) > arr.get(left)) {
+                minIdx = left;
+            }
+            if (right < arr.size() && arr.get(minIdx) > arr.get(right)) {
+                minIdx = right;
+            }
+
+            if (minIdx != i) {
+                int temp = arr.get(i);
+                arr.set(i, arr.get(minIdx));
+                arr.set(minIdx, temp);
+                heapify(minIdx);
+            }
+        }
+
+        public int remove() {
+            int data = arr.get(0);
+
+            int temp = arr.get(0);
+            arr.set(0, arr.get(arr.size() - 1));
+            arr.set(arr.size() - 1, temp);
+
+            arr.remove(arr.size() - 1);
+
+            heapify(0);
+            return data;
+        }
+
+        public boolean isEmpty() {
+            return arr.size() == 0;
+        }
+
     }
 
     public static void main(String[] args) {
+        Heap hp = new Heap();
+        hp.add(0);
+        hp.add(1);
+        hp.add(2);
 
+        while (!hp.isEmpty()) {
+            System.out.print(hp.peek() + " ");
+            hp.remove();
+        }
+        System.out.println();
     }
 
 }
