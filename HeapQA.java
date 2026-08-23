670c14585a2129e3e206c6cbc156e3617aa73761
harkit singh <harkitsinghsran9584@gmail.com>
2026-08-23 17:54:46 +0530

    notedown medium to hard level questions of heap or pirority queue

---
+++ HeapQA.java
@@ -0,0 +1,15 @@
+public class HeapQA {
+    // HeapSort
+
+    // Nearby cars
+
+    // Connect N Ropes
+
+    // Weakest Soldiers
+
+    // Sliding Window Maximum
+
+    public static void main(String[] args) {
+
+    }
+}
