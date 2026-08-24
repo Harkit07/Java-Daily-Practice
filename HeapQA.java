91cc4654657067ee64fc76b588acbaaef4d6e190
harkit singh <harkitsinghsran9584@gmail.com>
2026-08-24 08:53:00 +0530

    solve maximum sliding window question

--- HeapQA.java
+++ HeapQA.java
@@ -118,6 +118,42 @@ public class HeapQA {
     }
 
     // Sliding Window Maximum
+    static class Pair implements Comparable<Pair> {
+        int val;
+        int idx;
+
+        public Pair(int val, int idx) {
+            this.val = val;
+            this.idx = idx;
+        }
+
+        @Override
+        public int compareTo(Pair p2) {
+            // descending order
+            return p2.val - this.val;
+        }
+
+    }
+
+    public static void maxSildingWindows(int arr[], int k) {
+        int res[] = new int[arr.length - k + 1];
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        for (int i = 0; i < k; i++) {
+            pq.add(new Pair(arr[i], i));
+        }
+        res[0] = pq.peek().val;
+        for (int i = k; i < arr.length; i++) {
+            while (pq.size() > 0 && pq.peek().idx <= (i - k)) {
+                pq.remove();
+            }
+            pq.add(new Pair(arr[i], i));
+            res[i - k + 1] = pq.peek().val;
+        }
+        for (int i = 0; i < res.length; i++) {
+            System.out.print(res[i] + " ");
+        }
+        System.out.println();
+    }
 
     public static void main(String[] args) {
         // HeapSort
@@ -143,7 +179,8 @@ public class HeapQA {
         weakestSoilders(army, n);
 
         // Sliding Window Maximum
-        // int num[] = { 1, 3, -1, -3, 5, 3, 6, 7 };
-        // int s = 3;
+        int num[] = { 1, 3, -1, -3, 5, 3, 6, 7 };
+        int w = 3;
+        maxSildingWindows(num, w);
     }
 }
