49a703b8b9606d756ef004091259cb3db36da813
harkit singh <harkitsinghsran9584@gmail.com>
2026-08-26 09:09:01 +0530

    solve hashmap questions

--- HashMapQA.java
+++ HashMapQA.java
@@ -1,10 +1,48 @@
 // import java.util.LinkedHashMap;
 // import java.util.TreeMap;
 
+import java.util.HashMap;
+// import java.util.LinkedHashMap;
+
 public class HashMapQA {
-    // Majority Element
+    // Majority Element (arr.length/3)
+    public static void majorityElement(int nums[]) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            hm.put(nums[i], hm.getOrDefault(nums[i], 0) + 1);
+        }
+        for (int key : hm.keySet()) {
+            if (hm.get(key) > nums.length / 3) {
+                System.out.print(key + " ");
+            }
+        }
+        System.out.println();
+    }
 
     // Valid Anagram
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        HashMap<Character, Integer> hm = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            hm.put(ch, hm.getOrDefault(ch, 0) + 1);
+        }
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+            if (hm.get(ch) != null) {
+                if (hm.get(ch) == 1) {
+                    hm.remove(ch);
+                } else {
+                    hm.put(ch, hm.get(ch) - 1);
+                }
+            } else {
+                return false;
+            }
+        }
+        return hm.isEmpty();
+    }
 
     public static void main(String[] args) {
         // LinkedHashMap (Order of Insertion)
@@ -32,8 +70,13 @@ public class HashMapQA {
         // System.out.println();
 
         // Majority Element
+        int nums[] = { 1, 3, 2, 5, 1, 3, 1, 5, 1 };
+        majorityElement(nums);
 
         // Valid Anagram
+        String s = "knee";
+        String t = "keen";
+        System.out.println(isAnagram(s, t));
 
     }
 }
