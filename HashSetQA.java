2c2dd9d7fa7ab88e02e92a3585dccb6902eca9c0
harkit singh <harkitsinghsran9584@gmail.com>
2026-08-27 12:54:56 +0530

    solve hashset questions

--- HashSetQA.java
+++ HashSetQA.java
@@ -14,7 +14,26 @@ public class HashSetQA {
 
     // Union and Intersection
     public static void unionAndIntersection(int arr1[], int arr2[]) {
-
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < arr1.length; i++) {
+            set.add(arr1[i]);
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            set.add(arr2[i]);
+        }
+        System.out.println("Union =" + set.size());
+        set.clear();
+        for (int i = 0; i < arr1.length; i++) {
+            set.add(arr1[i]);
+        }
+        int count = 0;
+        for (int i = 0; i < arr2.length; i++) {
+            if (set.contains(arr2[i])) {
+                count++;
+                set.remove(arr2[i]);
+            }
+        }
+        System.out.println("Intersection =" + count);
     }
 
     public static void main(String[] args) {
@@ -61,6 +80,6 @@ public class HashSetQA {
         // Union and Intersection
         int arr1[] = { 7, 3, 9 };
         int arr2[] = { 6, 3, 9, 2, 9, 4 };
-
+        unionAndIntersection(arr1, arr2);
     }
 }
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,1 +1,1 @@
-
+Intersection
