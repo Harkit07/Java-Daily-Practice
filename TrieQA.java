2146ee714f326dfdfd62da6d133be440a98d4130
harkit singh <harkitsinghsran9584@gmail.com>
2026-08-27 19:04:11 +0530

    solve prefix sum problem question

--- TrieQA.java
+++ TrieQA.java
@@ -2,11 +2,13 @@ public class TrieQA {
     static class Node {
         Node children[] = new Node[26];
         boolean eow = false;
+        int freq;
 
         public Node() {
             for (int i = 0; i < 26; i++) {
                 children[i] = null;
             }
+            freq = 1;
         }
     }
 
@@ -53,7 +55,32 @@ public class TrieQA {
 
     // Prefix Problem
     public static void insertUtil(String str) {
+        Node curr = root;
+        for (int i = 0; i < str.length(); i++) {
+            int idx = str.charAt(i) - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            } else {
+                curr.children[idx].freq++;
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
 
+    public static void prefixProblem(Node root, String ans) {
+        if (root == null) {
+            return;
+        }
+        if (root.freq == 1) {
+            System.out.println(ans);
+            return;
+        }
+        for (int i = 0; i < root.children.length; i++) {
+            if (root.children[i] != null) {
+                prefixProblem(root.children[i], ans + (char) (i + 'a'));
+            }
+        }
     }
 
     // Starts With Problem
@@ -86,6 +113,11 @@ public class TrieQA {
         // Prefix Problem
         String arr[] = { "zebra", "dog", "duck", "dove" };
         // ans[]={"z", "dog", "du", "du"};
+        root.freq = -1;
+        for (int i = 0; i < arr.length; i++) {
+            insertUtil(arr[i]);
+        }
+        prefixProblem(root, "");
 
         // Starts With Problem
         String wordArr3[] = { "apple", "app", "mango", "man", "woman" };
