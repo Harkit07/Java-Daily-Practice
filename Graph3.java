d377c3207b141707e25483cfcdee916928a33f36
harkit singh <harkitsinghsran9584@gmail.com>
2026-08-30 15:13:48 +0530

    solve topological sort using BFS

--- Graph3.java
+++ Graph3.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Graph3 {
     static class Edge {
@@ -28,12 +30,12 @@ public class Graph3 {
         graph[5].add(new Edge(5, 2, 1));
     }
 
-    public static void checkAllPath(ArrayList<Edge> graph[]) {
+    public static void checkAllPath(ArrayList<Edge> graph[]) {// Directed Graph
         for (int i = 0; i < graph.length; i++) {
             graph[i] = new ArrayList<>();
         }
 
-        graph[0].add(new Edge(0, 1, 1));
+        graph[0].add(new Edge(0, 3, 1));
 
         graph[2].add(new Edge(2, 3, 1));
 
@@ -46,37 +48,63 @@ public class Graph3 {
         graph[5].add(new Edge(5, 2, 1));
     }
 
-    public static void checckDijkstra(ArrayList<Edge> graph[]) {
+    public static void checckDijkstra(ArrayList<Edge> graph[]) {// Directed Graph
         for (int i = 0; i < graph.length; i++) {
             graph[i] = new ArrayList<>();
         }
-        graph[0].add(new Edge(0, 1, 1));
-        graph[0].add(new Edge(0, 2, 1));
+        graph[0].add(new Edge(0, 1, 2));
+        graph[0].add(new Edge(0, 2, 4));
 
-        graph[1].add(new Edge(1, 0, 1));
-        graph[1].add(new Edge(1, 3, 1));
+        graph[1].add(new Edge(1, 2, 1));
+        graph[1].add(new Edge(1, 3, 7));
 
-        graph[2].add(new Edge(2, 0, 1));
-        graph[2].add(new Edge(2, 4, 1));
+        graph[2].add(new Edge(2, 4, 3));
 
-        graph[3].add(new Edge(3, 1, 1));
-        graph[3].add(new Edge(3, 4, 1));
         graph[3].add(new Edge(3, 5, 1));
 
-        graph[4].add(new Edge(4, 2, 1));
-        graph[4].add(new Edge(4, 3, 1));
-        graph[4].add(new Edge(4, 5, 1));
-
-        graph[5].add(new Edge(5, 3, 1));
-        graph[5].add(new Edge(5, 4, 1));
-        graph[5].add(new Edge(5, 6, 1));
-
-        graph[6].add(new Edge(6, 5, 1));
+        graph[4].add(new Edge(4, 3, 2));
+        graph[4].add(new Edge(4, 5, 5));
     }
 
     // Topological Sort - (BFS)
+    public static void calIndegree(ArrayList<Edge> graph[], int indeg[]) {
+        for (int i = 0; i < graph.length; i++) {
+            for (int j = 0; j < graph[i].size(); j++) {
+                Edge e = graph[i].get(j);
+                indeg[e.dest]++;
+            }
+        }
+    }
+
+    public static void topSortBFS(ArrayList<Edge> graph[]) {
+        int indeg[] = new int[graph.length];
+        calIndegree(graph, indeg);
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < indeg.length; i++) {
+            if (indeg[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        while (!q.isEmpty()) {
+            int curr = q.remove();
+            System.out.print(curr + " ");
+            for (int i = 0; i < graph[curr].size(); i++) {
+                Edge e = graph[curr].get(i);
+                indeg[e.dest]--;
+                if (indeg[e.dest] == 0) {
+                    q.add(e.dest);
+                }
+            }
+
+        }
+        System.out.println();
+    }
 
     // Print all Path - Directed Graph
+    public static void printPath(ArrayList<Edge> graph[], int src, int dest, String path) {
+
+    }
 
     // Dijkstra's Algorithm
 
@@ -85,6 +113,7 @@ public class Graph3 {
         int V = 7;
         ArrayList<Edge> graph[] = new ArrayList[V];
         checkTopSort(graph);
+        topSortBFS(graph);
 
     }
 }
