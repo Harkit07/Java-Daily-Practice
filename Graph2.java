b5c5d1e25621dd2d9b52626254bccdb5eb561843
harkit singh <harkitsinghsran9584@gmail.com>
2026-08-30 07:52:26 +0530

    solve all questions

--- Graph2.java
+++ Graph2.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 public class Graph2 {
     static class Edge {
@@ -64,12 +67,119 @@ public class Graph2 {
     }
 
     // Cycle Detection - Undirected Graph(Modified DFS)
+    public static boolean isCycleUndirected(ArrayList<Edge> graph[]) {
+        boolean vis[] = new boolean[graph.length];
+        for (int i = 0; i < graph.length; i++) {
+            if (!vis[i]) {
+                if (isCycleUndirectedUtil(graph, vis, i, -1)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
-    // Bipartite Graph
+    public static boolean isCycleUndirectedUtil(ArrayList<Edge> graph[], boolean vis[], int curr, int par) {
+        vis[curr] = true;
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            if (!vis[e.dest]) {
+                if (isCycleUndirectedUtil(graph, vis, e.dest, curr)) {
+                    return true;
+                }
+            } else if (vis[e.dest] && e.dest != par) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Bipartite Graph - (Modified BFS)
+    public static boolean isBipartite(ArrayList<Edge> graph[]) {
+        int col[] = new int[graph.length];
+        for (int i = 0; i < col.length; i++) {
+            col[i] = -1;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i < graph.length; i++) {
+            if (col[i] == -1) {
+                q.add(i);
+                col[i] = 0;
+                while (!q.isEmpty()) {
+                    int curr = q.remove();
+                    for (int j = 0; j < graph[curr].size(); j++) {
+                        Edge e = graph[curr].get(j);
+                        if (col[e.dest] == -1) {
+                            int nextCol = col[curr] == 0 ? 1 : 0;
+                            col[e.dest] = nextCol;
+                            q.add(e.dest);
+                        } else if (col[curr] == col[e.dest]) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
 
     // Cycle Detection - Directed Graph(Modified DFS)
+    public static boolean isCycledirected(ArrayList<Edge> graph[]) {
+        boolean vis[] = new boolean[graph.length];
+        boolean stack[] = new boolean[graph.length];
+        for (int i = 0; i < graph.length; i++) {
+            if (!vis[i]) {
+                if (isCycledirectedUtil(graph, i, vis, stack)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean isCycledirectedUtil(ArrayList<Edge> graph[], int curr, boolean vis[], boolean stack[]) {
+        vis[curr] = true;
+        stack[curr] = true;
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            if (stack[e.dest]) {
+                return true;
+            } else if (!vis[e.dest] && isCycledirectedUtil(graph, e.dest, vis, stack)) {
+                return true;
+            }
+        }
+        stack[curr] = false;
+        return false;
+    }
 
     // Topological Sort
+    public static void topSort(ArrayList<Edge> graph[]) {
+        boolean vis[] = new boolean[graph.length];
+        Stack<Integer> s = new Stack<>();
+        for (int i = 0; i < graph.length; i++) {
+            if (!vis[i]) {
+                topSortUtil(graph, i, vis, s);
+            }
+        }
+
+        while (!s.isEmpty()) {
+            System.out.print(s.pop() + " ");
+        }
+        System.out.println();
+    }
+
+    public static void topSortUtil(ArrayList<Edge> graph[], int curr, boolean vis[], Stack<Integer> s) {
+        vis[curr] = true;
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            if (!vis[e.dest]) {
+                topSortUtil(graph, e.dest, vis, s);
+            }
+        }
+        s.push(curr);
+    }
 
     @SuppressWarnings("unchecked")
     public static void main(String[] args) {
