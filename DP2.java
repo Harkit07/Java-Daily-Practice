68eb69d36ac9730c2cd5efb66fe5efa3acd42574
harkit singh <harkitsinghsran9584@gmail.com>
2026-09-04 15:13:43 +0530

    solve 0-1 knapsack with recursio, memoization and tabbulation

--- DP2.java
+++ DP2.java
@@ -1,9 +1,58 @@
 public class DP2 {
     // 0-1 Knapsack - Recursion
+    public static int knapsack(int val[], int wt[], int W, int n) {
+        if (W == 0 || n == 0) {
+            return 0;
+        }
+        if (wt[n - 1] <= W) {
+            return Math.max(val[n - 1] + knapsack(val, wt, W - wt[n - 1], n - 1), knapsack(val, wt, W, n - 1));
+        } else {
+            return knapsack(val, wt, W, n - 1);
+        }
+    }
 
     // 0-1 Knapsack - Memoization
+    public static int knapsackMemo(int val[], int wt[], int W, int n, int dp[][]) {
+        if (W == 0 || n == 0) {
+            return 0;
+        }
+        if (dp[n][W] != -1) {
+            return dp[n][W];
+        }
+        if (wt[n - 1] <= W) {
+            dp[n][W] = Math.max(val[n - 1] + knapsackMemo(val, wt, W - wt[n - 1], n - 1, dp),
+                    knapsackMemo(val, wt, W, n - 1, dp));
+            return dp[n][W];
+        } else {
+            dp[n][W] = knapsackMemo(val, wt, W, n - 1, dp);
+            return dp[n][W];
+        }
+    }
 
     // 0-1 Knapsack - Tabulation
+    public static int knapsackTab(int val[], int wt[], int W) {
+        int n = val.length;
+        int dp[][] = new int[n + 1][W + 1];
+
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = 0;
+        }
+        for (int i = 0; i < dp[0].length; i++) {
+            dp[0][i] = 0;
+        }
+
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (wt[i - 1] <= j) {
+                    dp[i][j] = Math.max(val[i - 1] + dp[i - 1][j - wt[i - 1]], dp[i - 1][j]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[n][W];
+    }
 
     // Target Sum Subset - Tabulation
 
@@ -14,10 +63,25 @@ public class DP2 {
         int val1[] = { 15, 14, 10, 45, 30 };
         int wt1[] = { 2, 5, 1, 3, 4 };
         int W1 = 7;
+        System.out.println(knapsack(val1, wt1, W1, val1.length));
 
         // 0-1 Knapsack - Memoization
+        int dp[][] = new int[val1.length + 1][W1 + 1];
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = 0;
+        }
+        for (int i = 0; i < dp[0].length; i++) {
+            dp[0][i] = 0;
+        }
+        System.out.println(knapsackMemo(val1, wt1, W1, val1.length, dp));
 
         // 0-1 Knapsack - Tabulation
+        System.out.println(knapsackTab(val1, wt1, W1));
 
         // Target Sum Subset - Tabulation
         int numbers[] = { 4, 2, 7, 1, 3 };
