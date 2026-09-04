d52ccb9cedffae70257af8f63c8b0a3110f0f84e
harkit singh <harkitsinghsran9584@gmail.com>
2026-09-04 12:45:13 +0530

    Note down DP part 2 problems

---
+++ DP2.java
@@ -0,0 +1,31 @@
+public class DP2 {
+    // 0-1 Knapsack - Recursion
+
+    // 0-1 Knapsack - Memoization
+
+    // 0-1 Knapsack - Tabulation
+
+    // Target Sum Subset - Tabulation
+
+    // Unbounded Knapsack - Tabulation
+
+    public static void main(String[] args) {
+        // 0-1 Knapsack - Recursion
+        int val1[] = { 15, 14, 10, 45, 30 };
+        int wt1[] = { 2, 5, 1, 3, 4 };
+        int W1 = 7;
+
+        // 0-1 Knapsack - Memoization
+
+        // 0-1 Knapsack - Tabulation
+
+        // Target Sum Subset - Tabulation
+        int numbers[] = { 4, 2, 7, 1, 3 };
+        int sum = 10; // Target Sum
+
+        // Unbounded Knapsack - Tabulation
+        int val2[] = { 15, 14, 10, 45, 30 };
+        int wt2[] = { 2, 5, 1, 3, 4 };
+        int W2 = 7;
+    }
+}
