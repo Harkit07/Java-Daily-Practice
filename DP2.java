d765581135ba0684360846326894ed7a0abe8c7b
harkit singh <harkitsinghsran9584@gmail.com>
2026-09-04 15:22:14 +0530

    solve target sum tabulation

--- DP2.java
+++ DP2.java
@@ -55,6 +55,26 @@ public class DP2 {
     }
 
     // Target Sum Subset - Tabulation
+    public static boolean targetSum(int arr[], int sum) {
+        int n = arr.length;
+        boolean dp[][] = new boolean[n + 1][sum + 1];
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = true;
+        }
+        for (int i = 0; i < dp[0].length; i++) {
+            dp[0][i] = false;
+        }
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (arr[i - 1] <= j && dp[i - 1][j - arr[i - 1]] == true) {
+                    dp[i][j] = true;
+                } else if (dp[i - 1][j] == true) {
+                    dp[i][j] = true;
+                }
+            }
+        }
+        return dp[n][sum];
+    }
 
     // Unbounded Knapsack - Tabulation
 
@@ -86,6 +106,7 @@ public class DP2 {
         // Target Sum Subset - Tabulation
         int numbers[] = { 4, 2, 7, 1, 3 };
         int sum = 10; // Target Sum
+        System.out.println(targetSum(numbers, sum));
 
         // Unbounded Knapsack - Tabulation
         int val2[] = { 15, 14, 10, 45, 30 };
