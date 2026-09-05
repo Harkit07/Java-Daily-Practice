44824ccf762ee0bb5138d61688e804ea93ec564f
harkit singh <harkitsinghsran9584@gmail.com>
2026-09-05 14:54:12 +0530

    solve unbounded tabulation

--- DP2.java
+++ DP2.java
@@ -77,6 +77,26 @@ public class DP2 {
     }
 
     // Unbounded Knapsack - Tabulation
+    public static int unboundedKnapsackTab(int val[], int wt[], int W) {
+        int n = val.length;
+        int dp[][] = new int[n + 1][W + 1];
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = 0;
+        }
+        for (int i = 0; i < dp[0].length; i++) {
+            dp[0][i] = 0;
+        }
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (wt[i - 1] <= j) {
+                    dp[i][j] = Math.max(val[i - 1] + dp[i][j - wt[i - 1]], dp[i - 1][j]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][W];
+    }
 
     public static void main(String[] args) {
         // 0-1 Knapsack - Recursion
@@ -112,5 +132,6 @@ public class DP2 {
         int val2[] = { 15, 14, 10, 45, 30 };
         int wt2[] = { 2, 5, 1, 3, 4 };
         int W2 = 7;
+        System.out.println(unboundedKnapsackTab(val2, wt2, W2));
     }
 }
