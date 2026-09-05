2dffdcd68cc2207cd86e9fe686cd03e9dc9c3771
harkit singh <harkitsinghsran9584@gmail.com>
2026-09-05 15:12:05 +0530

    solve rod cutting with tabulation

--- DP3.java
+++ DP3.java
@@ -22,6 +22,27 @@ public class DP3 {
     }
 
     // Rod Cutting - Tabulation
+    public static int rodCutting(int length[], int price[], int rodLength) {
+        int n = length.length;
+        int dp[][] = new int[n + 1][rodLength + 1];
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = 0;
+        }
+        for (int i = 0; i < dp[0].length; i++) {
+            dp[0][i] = 0;
+        }
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (length[i - 1] <= j) {
+                    dp[i][j] = Math.max(price[i - 1] + dp[i - 1][j - length[i - 1]], dp[i - 1][j]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[n][rodLength];
+    }
 
     // Longest Common Subsequence - Recursion
 
@@ -39,6 +60,7 @@ public class DP3 {
         int length[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
         int price[] = { 1, 5, 8, 9, 10, 17, 17, 20 };
         int rodLength = 8;
+        System.out.println(rodCutting(length, price, rodLength));
 
         // Longest Common Subsequence - Recursion
         String str1 = "abcde";
