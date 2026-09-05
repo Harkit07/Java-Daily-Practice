adffa105eee6dbd6460ae775266ad0c2aec91428
harkit singh <harkitsinghsran9584@gmail.com>
2026-09-05 15:23:58 +0530

    solve Longest Common Subsequence with Recursion, Memoization and Tabulation

--- DP3.java
+++ DP3.java
@@ -45,10 +45,57 @@ public class DP3 {
     }
 
     // Longest Common Subsequence - Recursion
+    public static int lcsRec(String str1, String str2, int n, int m) {
+        if (n == 0 || m == 0) {
+            return 0;
+        }
+        if (str1.charAt(n - 1) == str2.charAt(m - 1)) {
+            return lcsRec(str1, str2, n - 1, m - 1) + 1;
+        } else {
+            return Math.max(lcsRec(str1, str2, n - 1, m), lcsRec(str1, str2, n, m - 1));
+        }
+    }
 
     // Longest Common Subsequence - Memoization
+    public static int lcsMemo(String str1, String str2, int n, int m, int dp[][]) {
+        if (n == 0 || m == 0) {
+            return 0;
+        }
+        if (dp[n][m] != -1) {
+            return dp[n][m];
+        }
+        if (str1.charAt(n - 1) == str2.charAt(m - 1)) {
+            return dp[n][m] = lcsMemo(str1, str2, n - 1, m - 1, dp) + 1;
+        } else {
+            return dp[n][m] = Math.max(lcsMemo(str1, str2, n - 1, m, dp), lcsMemo(str1, str2, n, m - 1, dp));
+        }
+    }
 
     // Longest Common Subsequence - Tabulation
+    public static int lcsTab(String str1, String str2) {
+        int n = str1.length();
+        int m = str2.length();
+        int dp[][] = new int[n + 1][m + 1];
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
 
     public static void main(String[] args) {
         // Coin Change - Tabulation
@@ -65,10 +112,21 @@ public class DP3 {
         // Longest Common Subsequence - Recursion
         String str1 = "abcde";
         String str2 = "ace";
+        int n = str1.length();
+        int m = str2.length();
+        System.out.println(lcsRec(str1, str2, n, m));
 
         // Longest Common Subsequence - Memoization
+        int dp[][] = new int[n + 1][m + 1];
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        System.out.println(lcsMemo(str1, str2, n, m, dp));
 
         // Longest Common Subsequence - Tabulation
+        System.out.println(lcsTab(str1, str2));
 
     }
 }
