be2af1f4562fd4c628a0b30d277b17938e042ce1
harkit singh <harkitsinghsran9584@gmail.com>
2026-09-06 15:11:09 +0530

    note down dp part 4 questions

---
+++ DP4.java
@@ -0,0 +1,20 @@
+public class DP4 {
+    // Longest Common Substring - Tabulation
+
+    // Longest Increasing Subsequence - Tabulation
+
+    // Edit Distance - Tabulation
+
+    public static void main(String[] args) {
+        // Longest Common Substring - Tabulation
+        String s1 = "ABCDE";
+        String s2 = "ABGCE";
+
+        // Longest Increasing Subsequence - Tabulation
+        int arr[] = { 50, 3, 10, 7, 40, 80 };
+
+        // Edit Distance - Tabulation
+        String word1 = "intention";
+        String word2 = "execution";
+    }
+}
