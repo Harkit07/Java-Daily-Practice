c5c077d4cfa02efcac53d477459926e99e3fcd23
harkit singh <harkitsinghsran9584@gmail.com>
2026-09-06 15:56:42 +0530

    solve all problem

--- DP4.java
+++ DP4.java
@@ -17,7 +17,7 @@ public class DP4 {
         }
 
         for (int i = 1; i < dp.length; i++) {
-            for (int j = 1; j < dp.length; j++) {
+            for (int j = 1; j < dp[0].length; j++) {
                 if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                     ans = Math.max(ans, dp[i][j]);
@@ -79,10 +79,14 @@ public class DP4 {
         int dp[][] = new int[n + 1][m + 1];
 
         for (int i = 0; i < dp.length; i++) {
-            dp[0][i] = i;
-        }
-        for (int i = 0; i < dp[0].length; i++) {
-            dp[i][0] = i;
+            for (int j = 0; j < dp[0].length; j++) {
+                if (i == 0) {
+                    dp[i][j] = j;
+                }
+                if (j == 0) {
+                    dp[i][j] = i;
+                }
+            }
         }
 
         for (int i = 1; i < dp.length; i++) {
