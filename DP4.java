08f38635e805500df2f553f1fd3488e4469e56bd
harkit singh <harkitsinghsran9584@gmail.com>
2026-09-06 15:54:22 +0530

    solve all problem

--- DP4.java
+++ DP4.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class DP4 {
     // Longest Common Substring - Tabulation
     public static int longestCommonSubstring(String str1, String str2) {
@@ -28,8 +31,72 @@ public class DP4 {
     }
 
     // Longest Increasing Subsequence - Tabulation
+    public static int lcsTab(int arr1[], int arr2[]) {
+        int n = arr1.length;
+        int m = arr2.length;
+        int dp[][] = new int[n + 1][m + 1];
+
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = 0;
+        }
+        for (int i = 0; i < dp[0].length; i++) {
+            dp[0][i] = 0;
+        }
+
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (arr1[i - 1] == arr2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[n][m];
+
+    }
+
+    public static int lis(int arr1[]) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < arr1.length; i++) {
+            set.add(arr1[i]);
+        }
+        int arr2[] = new int[set.size()];
+        int i = 0;
+        for (int num : set) {
+            arr2[i] = num;
+            i++;
+        }
+        Arrays.sort(arr2);
+        return lcsTab(arr1, arr2);
+    }
 
     // Edit Distance - Tabulation
+    public static int editDistance(String str1, String str2) {
+        int n = str1.length();
+        int m = str2.length();
+        int dp[][] = new int[n + 1][m + 1];
+
+        for (int i = 0; i < dp.length; i++) {
+            dp[0][i] = i;
+        }
+        for (int i = 0; i < dp[0].length; i++) {
+            dp[i][0] = i;
+        }
+
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1])) + 1;
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
 
     public static void main(String[] args) {
         // Longest Common Substring - Tabulation
@@ -39,9 +106,11 @@ public class DP4 {
 
         // Longest Increasing Subsequence - Tabulation
         int arr1[] = { 50, 3, 10, 7, 40, 80 };
+        System.out.println(lis(arr1));
 
         // Edit Distance - Tabulation
         String word1 = "intention";
         String word2 = "execution";
+        System.out.println(editDistance(word1, word2));
     }
 }
