9e84c12dbd1518ee5be42390dad96a4f7fd806d9
harkit singh <harkitsinghsran9584@gmail.com>
2026-09-07 11:07:05 +0530

    note down dp part 5 all questions

---
+++ DP5.java
@@ -0,0 +1,38 @@
+public class DP5 {
+    // Wildlcard Matching - Tabulation
+
+    // Catalan Number - Recursion
+
+    // Catalan Number - Memoization
+
+    // Catalan Number - Tabulation
+
+    // Count BST's - Tabulation
+
+    // Mountain Ranges - Tabulation
+
+    public static void main(String[] args) {
+        // Wildlcard Matching - Tabulation
+        String text1 = "baaabab";
+        String pattern1 = "*****ba*****ab"; // true
+
+        String text2 = "baaabab";
+        String pattern2 = "a*ab"; // false
+
+        // Catalan Number - Recursion
+        int n = 4;
+
+        // Catalan Number - Memoization
+
+        // Catalan Number - Tabulation
+
+        // Count BST's - Tabulation
+        int pair1 = 3; // int pairs[]={10, 20, 30};
+        // ans = 5;
+
+        int pair2 = 4; // int pairs[]={10, 20, 30, 40};
+        // ans = 14;
+
+        // Mountain Ranges - Tabulation
+    }
+}
