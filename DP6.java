1c31ae0bae2bdbad4dac2ea99db3ffbb94eab003
harkit singh <harkitsinghsran9584@gmail.com>
2026-09-09 06:24:34 +0530

    practice DP part 6 questions

---
+++ DP6.java
@@ -0,0 +1,27 @@
+public class DP6 {
+    // Matrix Chain Multiplication - Recursion
+
+    // Matrix Chain Multiplication - Memoization
+
+    // Matrix Chain Multiplication - Tabulation
+
+    // Minimum Partitioning - Tabulation
+
+    // Minimum Array Jumps - Tabulation
+
+    public static void main(String[] args) {
+        // Matrix Chain Multiplication - Recursion
+        int arr1[] = { 1, 2, 3, 4, 3 };
+
+        // Matrix Chain Multiplication - Memoization
+
+        // Matrix Chain Multiplication - Tabulation
+
+        // Minimum Partitioning - Tabulation
+        int numbers[] = { 1, 6, 11, 5 }; // Min diff =1;
+
+        // Minimum Array Jumps - Tabulation
+        int arr2[] = { 2, 3, 1, 1, 4 }; // Min jump =2;
+
+    }
+}
