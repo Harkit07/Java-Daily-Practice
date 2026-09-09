7788200574e988750dc8dbe7594bd200a873e736
harkit singh <harkitsinghsran9584@gmail.com>
2026-09-09 09:17:33 +0530

    solve all DP part 6 questions

--- DP6.java
+++ DP6.java
@@ -1,27 +1,135 @@
+import java.util.Arrays;
+
 public class DP6 {
     // Matrix Chain Multiplication - Recursion
+    public static int mcmRec(int arr[], int i, int j) {
+        if (i == j) {
+            return 0;
+        }
+        int ans = Integer.MAX_VALUE;
+        for (int k = i; k <= j - 1; k++) {
+            int cost1 = mcmRec(arr, i, k);
+            int cost2 = mcmRec(arr, k + 1, j);
+            int cost3 = arr[i - 1] * arr[k] * arr[j];
+            ans = Math.min(ans, cost1 + cost2 + cost3);
+        }
+        return ans;
+    }
 
     // Matrix Chain Multiplication - Memoization
+    public static int mcmMemo(int arr[], int i, int j, int dp[][]) {
+        if (i == j) {
+            return 0;
+        }
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        int ans = Integer.MAX_VALUE;
+        for (int k = i; k <= j - 1; k++) {
+            int cost1 = mcmMemo(arr, i, k, dp);
+            int cost2 = mcmMemo(arr, k + 1, j, dp);
+            int cost3 = arr[i - 1] * arr[k] * arr[j];
+            ans = Math.min(ans, cost1 + cost2 + cost3);
+        }
+        return dp[i][j] = ans;
+    }
 
     // Matrix Chain Multiplication - Tabulation
+    public static int mcmTab(int arr[]) {
+        int n = arr.length;
+        int dp[][] = new int[n][n];
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][i] = 0;
+        }
+        for (int len = 2; len <= n - 1; len++) {
+            for (int i = 1; i <= n - len; i++) {
+                int j = i + len - 1;
+                dp[i][j] = Integer.MAX_VALUE;
+                for (int k = i; k <= j - 1; k++) {
+                    int cost1 = dp[i][k];
+                    int cost2 = dp[k + 1][j];
+                    int cost3 = arr[i - 1] * arr[k] * arr[j];
+                    dp[i][j] = Math.min(dp[i][j], cost1 + cost2 + cost3);
+                }
+            }
+        }
+        return dp[1][n - 1];
+    }
 
     // Minimum Partitioning - Tabulation
+    public static int minPartitioning(int arr[]) {
+        int n = arr.length;
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        int w = sum / 2;
+
+        int dp[][] = new int[n + 1][w + 1];
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = 0;
+        }
+        for (int i = 0; i < dp[0].length; i++) {
+            dp[0][i] = 0;
+        }
+
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (arr[i - 1] <= j) {
+                    dp[i][j] += Math.max(arr[i - 1] + dp[i - 1][j - arr[i - 1]], dp[i - 1][j]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        int sum1 = dp[n][w];
+        int sum2 = sum - sum1;
+        return Math.abs(sum1 - sum2);
+    }
 
     // Minimum Array Jumps - Tabulation
+    public static int minJumps(int arr[]) {
+        int n = arr.length;
+        int dp[] = new int[n];
+        Arrays.fill(dp, -1);
+        dp[n - 1] = 0;
+        for (int i = n - 2; i >= 0; i--) {
+            int steps = arr[i];
+            int ans = Integer.MAX_VALUE;
+            for (int j = i + 1; j <= i + steps && j < n; j++) {
+                if (dp[j] != -1) {
+                    ans = Math.min(ans, dp[j] + 1);
+                }
+            }
+            if (ans != Integer.MAX_VALUE) {
+                dp[i] = ans;
+            }
+        }
+        return dp[0];
+    }
 
     public static void main(String[] args) {
         // Matrix Chain Multiplication - Recursion
         int arr1[] = { 1, 2, 3, 4, 3 };
+        System.out.println(mcmRec(arr1, 1, arr1.length - 1));
 
         // Matrix Chain Multiplication - Memoization
+        int dp[][] = new int[arr1.length][arr1.length];
+        for (int i = 0; i < dp.length; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        System.out.println(mcmMemo(arr1, 1, arr1.length - 1, dp));
 
         // Matrix Chain Multiplication - Tabulation
+        System.out.println(mcmTab(arr1));
 
         // Minimum Partitioning - Tabulation
         int numbers[] = { 1, 6, 11, 5 }; // Min diff =1;
+        System.out.println(minPartitioning(numbers));
 
         // Minimum Array Jumps - Tabulation
         int arr2[] = { 2, 3, 1, 1, 4 }; // Min jump =2;
+        System.out.println(minJumps(arr2));
 
     }
 }
